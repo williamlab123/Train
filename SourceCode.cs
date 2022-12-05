3401caa77c55cef2dde7b1b4f3e8c809d03d6561
William Pedro <54008790+williamlab123@users.noreply.github.com>
2022-12-05 00:12:37 -0300

    Add files via upload

--- SourceCode.cs
+++ SourceCode.cs
@@ -3,7 +3,7 @@
 main();
 
 
-// Version 1.0
+// Version 2.0
 
 void main()
 {
@@ -15,18 +15,13 @@ int print()
 {
 
     int i;
-    for (i = 0; i <= 16; i++)
+    for (i = 0; i <= 22; i++)
     {
         switch (i)
         {
             case 1:
+                frame0();
 
-                Console.WriteLine(". . o O o O  O o O");
-                Console.WriteLine("                   o");
-                Console.WriteLine("         ______     o");
-                Console.WriteLine("____====  ]OO|_n_n__][_");
-                Console.WriteLine("[_______]_|__|__________)<");
-                Console.WriteLine(" oo   oo  'oo OOOO-| oo\\_");
                 Thread.Sleep(225);
                 Console.Clear();
 
@@ -35,12 +30,7 @@ int print()
 
             case 2:
 
-                Console.WriteLine(" . o O o O  O o O o");
-                Console.WriteLine("                    o");
-                Console.WriteLine("          ______     o");
-                Console.WriteLine(" ____====  ]OO|_n_n__][_");
-                Console.WriteLine(" [_______]_|__|_________)<");
-                Console.WriteLine("  oo   oo  'oo OOOO-| oo\\_");
+                frame1();
                 Thread.Sleep(225);
                 Console.Clear();
 
@@ -50,93 +40,56 @@ int print()
 
 
             case 3:
-              
-                Console.WriteLine("  o O o O  O o O o O");
-                Console.WriteLine("                     o");
-                Console.WriteLine("           ______     o");
-                Console.WriteLine("  ____====  ]OO|_n_n__][_");
-                Console.WriteLine("  [_______]_|__|_________)<");
-                Console.WriteLine("   oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+                frame2();
+                Thread.Sleep(225);
                 Console.Clear();
 
                 break;
 
             case 4:
-             
 
-                Console.WriteLine("   o O o O  O o O o O");
-                Console.WriteLine("                      o");
-                Console.WriteLine("            ______     o");
-                Console.WriteLine("   ____====  ]OO|_n_n__][_");
-                Console.WriteLine("   [_______]_|__|_________)<");
-                Console.WriteLine("    oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+                frame3();
+                Thread.Sleep(225);
                 Console.Clear();
 
                 break;
 
             case 5:
-             
-                Console.WriteLine("  o O o O  O o O o O o");
-                Console.WriteLine("                       o");
-                Console.WriteLine("            ______     o");
-                Console.WriteLine("    ____====  ]OO|_n_n__][_");
-                Console.WriteLine("    [_______]_|__|_________)<");
-                Console.WriteLine("     oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame4();
+                Thread.Sleep(225);
                 Console.Clear();
                 break;
             case 6:
-            
-                Console.WriteLine("  O o O  O o O o O o O");
-                Console.WriteLine("                        o");
-                Console.WriteLine("             ______     o");
-                Console.WriteLine("     ____====  ]OO|_n_n__][_");
-                Console.WriteLine("     [_______]_|__|_________)<");
-                Console.WriteLine("      oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame5();
+                Thread.Sleep(225);
                 Console.Clear();
 
                 break;
 
             case 7:
-           
-                Console.WriteLine(" O o O O o O o O o O o");
-                Console.WriteLine("                         o");
-                Console.WriteLine("              ______     o");
-                Console.WriteLine("      ____====  ]OO|_n_n__][_");
-                Console.WriteLine("      [_______]_|__|_________)<");
-                Console.WriteLine("       oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame6();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
                 break;
 
             case 8:
-              
-                Console.WriteLine(" o O O o O o O o O o o");
-                Console.WriteLine("                          o");
-                Console.WriteLine("               ______     o");
-                Console.WriteLine("       ____====  ]OO|_n_n__][_");
-                Console.WriteLine("       [_______]_|__|_________)<");
-                Console.WriteLine("        oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame7();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
                 break;
 
             case 9:
-             
-                Console.WriteLine(" oO O o O o O o O o o O");
-                Console.WriteLine("                           o");
-                Console.WriteLine("                ______     o");
-                Console.WriteLine("        ____====  ]OO|_n_n__][_");
-                Console.WriteLine("        [_______]_|__|_________)<");
-                Console.WriteLine("         oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame8();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
@@ -145,88 +98,118 @@ int print()
                 break;
 
             case 10:
-          
-                Console.WriteLine("O O o O o O o O o o O o O");
-                Console.WriteLine("                            o");
-                Console.WriteLine("                 ______     o");
-                Console.WriteLine("         ____====  ]OO|_n_n__][_");
-                Console.WriteLine("         [_______]_|__|_________)<");
-                Console.WriteLine("          oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+
+                frame9();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
                 break;
 
             case 11:
-               Console.WriteLine("O O o O o O o O o o O o O o ");
-                Console.WriteLine("                             o");
-                Console.WriteLine("                  ______     o");
-                Console.WriteLine("          ____====  ]OO|_n_n__][_");
-                Console.WriteLine("          [_______]_|__|_________)<");
-                Console.WriteLine("           oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+
+                frame10();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
 
                 break;
             case 12:
-               Console.WriteLine("O O o O o O o O o o O o O  o");
-                Console.WriteLine("                            o");
-                Console.WriteLine("                   ______     o");
-                Console.WriteLine("           ____====  ]OO|_n_n__][_");
-                Console.WriteLine("           [_______]_|__|_________)<");
-                Console.WriteLine("            oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame11();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
 
                 break;
             case 13:
-              Console.WriteLine("O O o O o O o O o o O o O o o");
-                Console.WriteLine("                             o");
-                Console.WriteLine("                    ______     o");
-                Console.WriteLine("            ____====  ]OO|_n_n__][_");
-                Console.WriteLine("            [_______]_|__|_________)<");
-                Console.WriteLine("             oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame12();
+                Thread.Sleep(225);
                 Console.Clear();
 
 
 
                 break;
             case 14:
-                  Console.WriteLine("O O o O o O o o O o o O o O");
-                Console.WriteLine("                              o");
-                Console.WriteLine("                     ______     o");
-                Console.WriteLine("             ____====  ]OO|_n_n__][_");
-                Console.WriteLine("             [_______]_|__|_________)<");
-                Console.WriteLine("              oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+                frame13();
+                Thread.Sleep(225);
                 Console.Clear();
                 break;
 
             case 15:
-                      Console.WriteLine(" o O  o O o o O o o O o O");
-                Console.WriteLine("                                o");
-                Console.WriteLine("                     ______     o");
-                Console.WriteLine("             ____====  ]OO|_n_n__][_");
-                Console.WriteLine("             [_______]_|__|_________)<");
-                Console.WriteLine("              oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+
+                frame14();
+                Thread.Sleep(225);
                 Console.Clear();
                 break;
 
             case 16:
-                       Console.WriteLine(" o o O o O o o O o o O o O");
-                Console.WriteLine("                                  o");
-                Console.WriteLine("                       ______     o");
-                Console.WriteLine("               ____====  ]OO|_n_n__][_");
-                Console.WriteLine("               [_______]_|__|_________)<");
-                Console.WriteLine("                oo   oo  'oo OOOO-| oo\\_");
-                  Thread.Sleep(225);
+
+
+                frame15();
+                Thread.Sleep(225);
+                Console.Clear();
+
+                break;
+
+            case 17:
+
+
+                frame16();
+                Thread.Sleep(225);
+                Console.Clear();
+
+                break;
+
+            case 18:
+
+
+                frame17();
+                Thread.Sleep(225);
+                Console.Clear();
+
+                break;
+
+            case 19:
+
+
+                frame18();
+                Thread.Sleep(225);
+                Console.Clear();
+
+                break;
+
+            case 20:
+
+
+                frame19();
+                Thread.Sleep(225);
+                Console.Clear();
+
+                break;
+
+            case 21:
+
+
+                frame20();
+                Thread.Sleep(225);
+                Console.Clear();
+
+                break;
+
+
+            case 22:
+
+
+                frame21();
+                Thread.Sleep(225);
                 Console.Clear();
 
                 break;
@@ -242,4 +225,270 @@ int print()
 
 
 }
+
+
+void frame0()
+{
+
+    Console.WriteLine(". . o O o O  O o O");
+    Console.WriteLine("                   o");
+    Console.WriteLine("         ______     o");
+    Console.WriteLine("____====  ]OO|_n_n__][_");
+    Console.WriteLine("[_______]_|__|__________)<");
+    Console.WriteLine(" oo   oo  'oo OOOO-| oo\\_");
+}
+
+void frame1()
+{
+    Console.WriteLine(" . o O o O  O o O o");
+    Console.WriteLine("                    o");
+    Console.WriteLine("          ______     o");
+    Console.WriteLine(" ____====  ]OO|_n_n__][_");
+    Console.WriteLine(" [_______]_|__|_________)<");
+    Console.WriteLine("  oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame2()
+{
+
+    Console.WriteLine("  o O o O  O o O o O");
+    Console.WriteLine("                     o");
+    Console.WriteLine("           ______     o");
+    Console.WriteLine("  ____====  ]OO|_n_n__][_");
+    Console.WriteLine("  [_______]_|__|_________)<");
+    Console.WriteLine("   oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame3()
+{
+
+    Console.WriteLine("   o O o O  O o O o O");
+    Console.WriteLine("                      o");
+    Console.WriteLine("            ______     o");
+    Console.WriteLine("   ____====  ]OO|_n_n__][_");
+    Console.WriteLine("   [_______]_|__|_________)<");
+    Console.WriteLine("    oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame4()
+{
+    Console.WriteLine("  o O o O  O o O o O o");
+    Console.WriteLine("                       o");
+    Console.WriteLine("            ______     o");
+    Console.WriteLine("    ____====  ]OO|_n_n__][_");
+    Console.WriteLine("    [_______]_|__|_________)<");
+    Console.WriteLine("     oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame5()
+{
+    Console.WriteLine("  O o O  O o O o O o O");
+    Console.WriteLine("                        o");
+    Console.WriteLine("             ______     o");
+    Console.WriteLine("     ____====  ]OO|_n_n__][_");
+    Console.WriteLine("     [_______]_|__|_________)<");
+    Console.WriteLine("      oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame6()
+{
+    Console.WriteLine(" O o O O o O o O o O o");
+    Console.WriteLine("                         o");
+    Console.WriteLine("              ______     o");
+    Console.WriteLine("      ____====  ]OO|_n_n__][_");
+    Console.WriteLine("      [_______]_|__|_________)<");
+    Console.WriteLine("       oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame7()
+{
+
+    Console.WriteLine(" o O O o O o O o O o o");
+    Console.WriteLine("                          o");
+    Console.WriteLine("               ______     o");
+    Console.WriteLine("       ____====  ]OO|_n_n__][_");
+    Console.WriteLine("       [_______]_|__|_________)<");
+    Console.WriteLine("        oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame8()
+{
+
+    Console.WriteLine(" oO O o O o O o O o o O");
+    Console.WriteLine("                           o");
+    Console.WriteLine("                ______     o");
+    Console.WriteLine("        ____====  ]OO|_n_n__][_");
+    Console.WriteLine("        [_______]_|__|_________)<");
+    Console.WriteLine("         oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame9()
+{
+    Console.WriteLine("O O o O o O o O o o O o O");
+    Console.WriteLine("                            o");
+    Console.WriteLine("                 ______     o");
+    Console.WriteLine("         ____====  ]OO|_n_n__][_");
+    Console.WriteLine("         [_______]_|__|_________)<");
+    Console.WriteLine("          oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame10()
+{
+    Console.WriteLine("O O o O o O o O o o O o O o ");
+    Console.WriteLine("                             o");
+    Console.WriteLine("                  ______     o");
+    Console.WriteLine("          ____====  ]OO|_n_n__][_");
+    Console.WriteLine("          [_______]_|__|_________)<");
+    Console.WriteLine("           oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame11()
+{
+    Console.WriteLine("O O o O o O o O o o O o O  o");
+    Console.WriteLine("                            o");
+    Console.WriteLine("                   ______     o");
+    Console.WriteLine("           ____====  ]OO|_n_n__][_");
+    Console.WriteLine("           [_______]_|__|_________)<");
+    Console.WriteLine("            oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame12()
+{
+    Console.WriteLine("O O o O o O o O o o O o O o o");
+    Console.WriteLine("                             o");
+    Console.WriteLine("                    ______     o");
+    Console.WriteLine("            ____====  ]OO|_n_n__][_");
+    Console.WriteLine("            [_______]_|__|_________)<");
+    Console.WriteLine("             oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame13()
+{
+    Console.WriteLine("O O o O o O o o O o o O o O");
+    Console.WriteLine("                              o");
+    Console.WriteLine("                     ______     o");
+    Console.WriteLine("             ____====  ]OO|_n_n__][_");
+    Console.WriteLine("             [_______]_|__|_________)<");
+    Console.WriteLine("              oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame14()
+{
+    Console.WriteLine(" o O  o O o o O o o O o O");
+    Console.WriteLine("                                o");
+    Console.WriteLine("                     ______     o");
+    Console.WriteLine("             ____====  ]OO|_n_n__][_");
+    Console.WriteLine("             [_______]_|__|_________)<");
+    Console.WriteLine("              oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame15()
+{
+    Console.WriteLine(" o o O o O o o O o o O o O");
+    Console.WriteLine("                                  o");
+    Console.WriteLine("                       ______     o");
+    Console.WriteLine("               ____====  ]OO|_n_n__][_");
+    Console.WriteLine("               [_______]_|__|_________)<");
+    Console.WriteLine("                oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame16()
+{
+    Console.WriteLine(" o o O o O o o O o o O o O o");
+    Console.WriteLine("                                   o");
+    Console.WriteLine("                        ______     o");
+    Console.WriteLine("                ____====  ]OO|_n_n__][_");
+    Console.WriteLine("                [_______]_|__|_________)<");
+    Console.WriteLine("                 oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+void frame17()
+{
+    Console.WriteLine("  o O o O o o O o o O o O o o");
+    Console.WriteLine("                                    o");
+    Console.WriteLine("                         ______     o");
+    Console.WriteLine("                 ____====  ]OO|_n_n__][_");
+    Console.WriteLine("                 [_______]_|__|_________)<");
+    Console.WriteLine("                  oo   oo  'oo OOOO-| oo\\_");
+}
+
+void frame18()
+{
+    Console.WriteLine(" o  o O o O o o O o o O o O o");
+    Console.WriteLine("                                    o");
+    Console.WriteLine("                          ______     o");
+    Console.WriteLine("                  ____====  ]OO|_n_n__][_");
+    Console.WriteLine("                  [_______]_|__|_________)<");
+    Console.WriteLine("                   oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+
+
+
+void frame19()
+{
+    Console.WriteLine(" o o O o O o o O o o O o O o o");
+    Console.WriteLine("                                     o");
+    Console.WriteLine("                           ______     o");
+    Console.WriteLine("                   ____====  ]OO|_n_n__][_");
+    Console.WriteLine("                   [_______]_|__|_________)<");
+    Console.WriteLine("                    oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+
+
+
+void frame20()
+{
+    Console.WriteLine("  o O o O o o O o o O o O  O o");
+    Console.WriteLine("                                      o");
+    Console.WriteLine("                            ______     o");
+    Console.WriteLine("                    ____====  ]OO|_n_n__][_");
+    Console.WriteLine("                    [_______]_|__|_________)<");
+    Console.WriteLine("                     oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+
+
+
+void frame21()
+{
+    Console.WriteLine(" o o O o O o o O o o O o O o O O");
+    Console.WriteLine("                                       o");
+    Console.WriteLine("                             ______     o");
+    Console.WriteLine("                     ____====  ]OO|_n_n__][_");
+    Console.WriteLine("                     [_______]_|__|_________)<");
+    Console.WriteLine("                      oo   oo  'oo OOOO-| oo\\_");
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
